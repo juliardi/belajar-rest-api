46f5128be28a0c4c51c9f0727d54a748d579a649
juliardi <ardi93@gmail.com>
2024-06-04 11:17:40 +0700

    menambahkan API untuk menambahkan data mahasiswa

--- example/routes/api.php
+++ example/routes/api.php
@@ -16,6 +16,8 @@ $router->get('/students', function($params, $container) {
     $statement->execute();
     $result = $statement->fetchAll(PDO::FETCH_ASSOC);
 
+    header('Content-Type: application/json');
+
     echo json_encode($result);
 });
 
@@ -39,4 +41,28 @@ $router->get('/students/{nim}', function($params, $container) {
 
     http_response_code(404);
     echo json_encode($result);
+});
+
+$router->post('/students', function($params, $container) {
+    $data = $_POST['mahasiswa'];
+    $pdo = $container->get('pdo');
+
+    $statement = $pdo->prepare("INSERT INTO students (nim, name, address, birthdate, faculty_id, created_at) VALUES (:nim, :name, :address, :birthdate, :faculty_id, :created_at)");
+
+    $statement->bindParam(':nim', $data['nim']);
+    $statement->bindParam(':name', $data['name']);
+    $statement->bindParam(':address', $data['address']);
+    $statement->bindParam(':birthdate', $data['birthdate']);
+    $statement->bindParam(':faculty_id', $data['faculty_id']);
+    $statement->bindParam(':created_at', date('Y-m-d H:i:s'));
+
+    $statement->execute();
+
+    header('Content-Type: application/json');
+
+    http_response_code(201);
+
+    echo json_encode([
+        'id' => $pdo->lastInsertId(),
+    ]);
 });
